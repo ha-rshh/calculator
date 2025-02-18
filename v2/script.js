d166ad51efccbe375a5dad030b531434b36cdb92
ha-rshh <prajapati.captain@gmail.com>
2025-02-18 16:52:44 +0530

    removed previous code starting from scratch

--- v2/script.js
+++ v2/script.js
@@ -6,30 +6,21 @@ const operationsBtn = document.querySelectorAll(".operation");
 const allClearBtn = document.querySelector(".all-clear");
 const deleteBtn = document.querySelector(".delete");
 const equalsBtn = document.querySelector(".equals");
-const previousOperandTextElement = document.querySelector("previous-operand");
-const currentOperandTextElement = document.querySelector("current-operand");
+const previousOperandTextElement = document.getElementById("previous-operand");
+const currentOperandTextElement = document.getElementById("current-operand");
 const themeToggleBtn = document.querySelector(".theme-toggle");
 
-/*
- * clear()             -> Clears the input
- * delete()            -> deletes the currentOperand's last charachter
- * appendNumber()      -> add whatever is sent as input (lookout for '.')
- * chooseOperation()   -> updates the operation and sets the current operand. If the operand is same as the previous on then we compute the value
- * compute()           -> computes the value based on the operation
- * getDisplayNumber()  -> returns the number to be displayed
- * updateDisplay()     -> will use the getDisplayNumber() function and update the DOM
- */
-
-let currentOperand = "",
-  previousOperand = "",
-  operation = "";
+let currentOperand = "";
+let previousOperand = "";
+let operation = "";
 
 function clear() {
   currentOperand = "";
   previousOperand = "";
-  operation = "";
+  operation = undefined;
 }
 
-function deleteChar() {
-    currentOperand = currentOperand.toString().slice(0, -1)
-}
+numbersBtn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+   
+});
